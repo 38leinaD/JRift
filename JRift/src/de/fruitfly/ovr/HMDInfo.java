1100506dc9e1afd95d453edcd8112bffcf31ecb2
StellaArtois <pete.tabram@gmail.com>
2013-05-12 21:23:44 +0100

    JRift library as used by Minecrift 0.22. JRift V1.1

--- JRift/src/de/fruitfly/ovr/HMDInfo.java
+++ JRift/src/de/fruitfly/ovr/HMDInfo.java
@@ -9,9 +9,10 @@ public class HMDInfo {
 	public float VScreenSize;
 	public float VScreenCenter;
 	public float EyeToScreenDistance;
-	public float LensSeperationDistance;
+	public float LensSeparationDistance;
 	public float InterpupillaryDistance;
 	public float[] DistortionK = new float[4];
+    public float[] ChromaticAb = new float[4];
 	
 	public int DesktopX; // Not queried from OVR AP yet
 	public int DesktopY; // Not queried from OVR API yet
@@ -25,9 +26,10 @@ public class HMDInfo {
 				+ ", VScreenSize=" + VScreenSize + ", VScreenCenter="
 				+ VScreenCenter + ", EyeToScreenDistance="
 				+ EyeToScreenDistance + ", LensSeperationDistance="
-				+ LensSeperationDistance + ", InterpupillaryDistance="
+				+ LensSeparationDistance + ", InterpupillaryDistance="
 				+ InterpupillaryDistance + ", DistortionK="
-				+ Arrays.toString(DistortionK) + ", DesktopX=" + DesktopX
+				+ Arrays.toString(DistortionK) + ", ChromaticAb="
+                + Arrays.toString(ChromaticAb) + ", DesktopX=" + DesktopX
 				+ ", DesktopY=" + DesktopY + ", DisplayDeviceName="
 				+ DisplayDeviceName + ", DisplayId=" + DisplayId + "]";
 	}
--- JRift/src/de/fruitfly/ovr/IOculusRift.java
+++ JRift/src/de/fruitfly/ovr/IOculusRift.java
@@ -1,10 +1,19 @@
 package de.fruitfly.ovr;
 
 public interface IOculusRift {
+    public String getVersion();
 	public boolean init();
 	public boolean isInitialized();
 	public void poll();
+    public float getYawDegrees_LH();
+    public float getPitchDegrees_LH();
+    public float getRollDegrees_LH();
 	public HMDInfo getHMDInfo();
 	public SensorInfo getSensorInfo();
+    public EyeRenderParams getEyeRenderParams(int viewPortX, int viewPortY, int viewPortWidth, int viewPortHeight, float nearClip, float farClip);
+    public EyeRenderParams getEyeRenderParams(int viewPortWidth, int viewPortHeight);
 	public void destroy();
+    public void setIPD(float ipd);
+    public float getIPD();
+    public void setPrediction(float delta, boolean enable);
 }

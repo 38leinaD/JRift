9b49522952180ef5966e5b4a6698d6028c222157
StellaArtois <pete.tabram@gmail.com>
2013-05-21 19:19:48 +0100

    Ensure IPD is set within Stereo Eye Params.

--- JRiftLibrary/JRiftLibrary/JRiftLibrary.cpp
+++ JRiftLibrary/JRiftLibrary/JRiftLibrary.cpp
@@ -265,10 +265,8 @@ JNIEXPORT jobject JNICALL Java_de_fruitfly_ovr_OculusRift__1getEyeRenderParams(
 	{
 		stereo.SetHMDInfo(Info);
 	}
-	else
-	{
-		stereo.SetIPD(_ipd);
-	}
+
+	stereo.SetIPD(_ipd); // Set IPD manually
 	stereo.SetEyeToScreenDistanceScaleFactor(eyeToScreenDistanceScaleFactor); // FOV adjustment
 	stereo.SetStereoMode(Util::Render::Stereo_LeftRight_Multipass);
 	stereo.SetFullViewport(viewPort);
